4e6135dac361f8a6b8e6ed6002f38700a82f156f
Ori Pekelman <ori+github@pekelman.com>
2015-06-10 08:50:43 +0200

    php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 // We get the relationships form the platform environement
 $relationships = json_decode(base64_decode($_ENV['PLATFORM_RELATIONSHIPS']), TRUE);
+
+echo "<html><body><h4>Platform.sh minimal example with DB connectivity</h4>Here are the relationships for this app:<pre>".base64_decode($_ENV['PLATFORM_RELATIONSHIPS'])."</pre><pre>";
+
 // We are using the first database found in your relationships.
 $link = mysql_connect($relationships['database'][0]['host'], $relationships['database'][0]['username'], $relationships['database'][0]['password'])
     or die('Could not connect: ' . mysql_error());
@@ -15,4 +18,5 @@ $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 $query = 'SELECT * from users;';
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 echo("Created table inserted values here are the results:");
-var_dump($result);
+echo mysql_result($result, 0);
+echo "</pre></body></html>"

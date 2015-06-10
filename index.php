56748915d81c2b4adc35af63795886c9c7a0e331
Ori Pekelman <ori+github@pekelman.com>
2015-06-10 08:42:51 +0200

    php

--- index.php
+++ index.php
@@ -8,9 +8,12 @@ echo 'Connected successfully';
 mysql_select_db($relationships['database'][0]['path']) or die('Could not select database');
 
 // Performing SQL query
-$query = 'CREATE TABLE users (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL);  INSERT INTO users(name) VALUES ("john"); SELECT * from users ;';
+$query = 'CREATE TABLE users (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL);';
+INSERT INTO users(name) VALUES ("john"); SELECT * from users ;
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
-
-echo("Created table inserted values here is the result:");
-
+$query = 'INSERT INTO users(name) VALUES ("john");';
+$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+$query = 'SELECT * from users;';
+$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+echo("Created table inserted values here are the results:");
 var_dump($result);
